75a4d1c9aa8709fe7152eddeb6501f4477b0e785
G2 <jithraj97@gmail.com>
2019-02-10 01:12:09 +0530

    on the track

--- example.js
+++ example.js
@@ -18,7 +18,7 @@ const web=new WebClient(token);
 
 
 app.post('/',(req,res)=>{
-    console.log(req.body['payload']);
+    console.log(req['payload']);
     // res.sendStatus(200);
     // fs.writeFileSync("result.text",stringify(req.body,undefined,2));
     res.send(req.body);
