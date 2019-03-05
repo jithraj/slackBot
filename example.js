b6907bf8ebbef3e2d48c4e9c0ce81833f8da602d
G2 <jithraj97@gmail.com>
2019-03-06 01:35:02 +0530

    on the way

--- example.js
+++ example.js
@@ -24,7 +24,7 @@ app.use('/', slackInteractions.expressMiddleware());
 
 app.post('/',(req,res)=>{
     //console.log(JSON.parse(req.body.payload));
-    console.log(JSON.parse(req.body['payload'])['response_url']);
+    console.log(JSON.parse(req.body['payload']));
     //console.log(stringify(req,undefined,2));
     // res.sendStatus(200);
     // fs.writeFileSync("result.text",stringify(req.body,undefined,2));
