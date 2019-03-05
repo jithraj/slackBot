0e9e51afe4d38a5288e0eb2ce8905368d0f96eac
G2 <jithraj97@gmail.com>
2019-03-06 01:42:23 +0530

    on the way

--- example.js
+++ example.js
@@ -24,7 +24,7 @@ const web=new WebClient(token);
 
 app.post('/',(req,res)=>{
     //console.log(JSON.parse(req.body.payload));
-    console.log('killer');
+    console.log('killers');
     console.log(JSON.parse(req.body['payload'])['response_url']);
     //console.log(stringify(req,undefined,2));
     // res.sendStatus(200);
