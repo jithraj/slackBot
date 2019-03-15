5e3e801b7e46a70c7420ac4a897db3cfc8c266ba
G2 <jithraj97@gmail.com>
2019-03-16 01:24:57 +0530

    On the progress

--- example.js
+++ example.js
@@ -24,8 +24,8 @@ const web=new WebClient(token);
 app.post('/',(req,res)=>{
     //console.log(JSON.parse(req.body));
     console.log('killers@work');
-    //console.log(JSON.parse(req.body['payload'])['response_url']);
-    console.log(stringify(req,undefined,2));
+    console.log(JSON.parse(req.body['payload'])['response_url']);
+    //console.log(stringify(req,undefined,2));
     // res.sendStatus(200);
     // fs.writeFileSync("result.text",stringify(req.body,undefined,2));
     res.send(`Request Body ${req.payload}`);
