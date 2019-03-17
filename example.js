9ed0eacfea4e9eb76edb62e701d0f01dd638f7bb
G2 <jithraj97@gmail.com>
2019-03-18 01:39:16 +0530

    On to the progress

--- example.js
+++ example.js
@@ -43,8 +43,8 @@ rtm.on('message', (message) => {
             debugger;
 
             axios.get(`https://evening-brook-60598.herokuapp.com/?q=${message.text}`).then(function (response) {
-               console.log(JSON.stringify(message,undefined,2));     
-               console.log(response);
+               //console.log(JSON.stringify(message,undefined,2));     
+               //console.log(response);
             })
             .catch(function (error) {
                  console.log(error);
