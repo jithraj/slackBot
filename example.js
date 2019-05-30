5f9e9c846b12cd0f240c791aa8156d8d90f88a0d
G2 <jithraj97@gmail.com>
2019-05-30 14:16:23 +0530

    About to finish

--- example.js
+++ example.js
@@ -15,7 +15,7 @@ const fs=require("fs");
 
 const slackInteractions = createMessageAdapter('f39a58a498ddbde21ba757ee89d333ad');
 var token='xoxb-430629803749-502830627862-kFrQxFm0F4AIZh2ddBStx9H4';
-var token1='f39a58a498ddbde21ba757ee89d333ad'
+var token1=createMessageAdapter('f39a58a498ddbde21ba757ee89d333ad');
 var port=process.env.PORT || 3000;
 
 var express=require('express');
